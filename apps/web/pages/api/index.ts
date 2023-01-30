79f0cd540c79ca23be01317661f14d62ba6b2d72
rodas-j <se.rodas.workicho@gmail.com>
2023-01-29 19:28:44 -0500

    done

---
+++ apps/web/pages/api/index.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const { Configuration, OpenAIApi } = require("openai");
+require("dotenv").config();
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const PREFACE = `Turn the following into latex:`;
+  const prompt = req.query.prompt;
+  const SUFFIX = "OUTPUT:\n";
+  const fullPrompt = PREFACE + "\n" + prompt + "\n" + SUFFIX;
+  console.log(prompt);
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: fullPrompt,
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+  const data = response.data.choices[0].text;
+
+  res.status(200).json({ data: data });
+}
+
+export default handler;
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,58 @@
 import { Button } from "ui";
+import Hero from "../components/hero/Hero";
+import { useEffect, useState } from "react";
+
+async function transcribe(text: string) {
+  let requestOptions = {
+    method: "GET",
+    redirect: "follow",
+  };
+  const response = await fetch(
+    `/api?prompt=${text}`,
+    requestOptions as RequestInit
+  );
+  return await response.json();
+}
 
 export default function Web() {
+  const [text, setText] = useState("");
+  const [latex, setLatex] = useState("");
+  const [loading, setLoading] = useState(false);
+  const handleTranscribe = async () => {
+    setLoading(true);
+    const response = await transcribe(text);
+    setLatex(response.data);
+    setLoading(false);
+  };
+
   return (
-    <div>
-      <h1 className="bg-blue-100">Web</h1>
-    </div>
+    <>
+      <div className="flex h-screen justify-center items-center">
+        <div className="flex-col justify-between min-w-lg">
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Write normal text here..."
+            className="textarea textarea-bordered textarea-md h-44 w-full"
+          ></textarea>
+
+          <button onClick={handleTranscribe} className="btn btn-outlinel my-4">
+            {loading && (
+              <svg
+                className="bg-white animate-spin h-4 w-4 mr-3 ..."
+                viewBox="0 0 24 24"
+              ></svg>
+            )}
+            Transcribe
+          </button>
+
+          <textarea
+            value={latex}
+            placeholder="Latex will appear here..."
+            className="textarea textarea-bordered textarea-md h-44 w-full"
+          ></textarea>
+        </div>
+      </div>
+    </>
   );
 }
