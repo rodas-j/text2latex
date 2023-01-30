0a76f3d6a24d049ebea2c731e417f91f83337925
rodas-j <se.rodas.workicho@gmail.com>
2023-01-29 21:02:45 -0500

    add analytics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "../tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        defer
+        data-domain="text2latex.com"
+        src="https://plausible.io/js/script.js"
+      />
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
