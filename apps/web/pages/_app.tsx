d54dd3ae73a2465936dca12e1b3c795cb088aa2e
rodas-j <50768698+rodas-j@users.noreply.github.com>
2024-02-05 19:13:15 +0000

    Add posthog analytics

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,11 @@
 import type { AppProps } from "next/app";
 import Script from "next/script";
 import "../tailwind.css";
+import posthog from 'posthog-js'
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  posthog.init('phc_gnJxS3xB23ajulr5CYQa08YAgf4h3KitxW4wuwbpGdX', { api_host: 'https://app.posthog.com' });
   return (
     <>
       <Script
